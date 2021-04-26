d09946ce394588d1f876de1c30d91f3a2f2813ec
daniel rangel <dr@daniels-MacBook-Pro.local>
2021-04-25 23:32:13 -0400

    added mongo and sendgrid

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { response } = require('express');
+//require('dotenv').config()
 const express  = require('express');
 const app =  express();
-
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000;
 
 ////Middeware
@@ -9,32 +9,99 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
-//database
-const Datastore = require('nedb')
-const db = new Datastore('./database.db')
-db.loadDatabase(error => console.log(error))
+const connectDB = async ()=>{
+    try {
+       const conn = await mongoose.connect(process.env.MONGO_DB, {
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+           useFindAndModify: false
+        })
 
-app.post('/rsvp', (req, resp)=>{
-    db.insert({...req.body}, (error, data)=>{
-        console.log(data)
-    })
-    resp.redirect('/')
-})
-
-app.get('/rsvp', (req, resp)=>{
-    db.find({}, (error, data)=>{
-        console.log(data)
-        resp.json(data)
-    })
-})
-
-app.listen(PORT, (error)=>
-{
-    console.log(`listening on ${PORT}`)
-    if (error)
-    {
-        console.log(error)
+        console.log(`mongoDB is connected to: ${conn.connection.host}`)
+    } catch (error) {
+        console.error(error);
+        process.exit(1)
     }
-})
+}
 
-console.log(__dirname)
+connectDB();
+
+module.exports = connectDB;
+
+const rsvpSchema = new mongoose.Schema({
+    name: String,
+    count: Number,
+    message: String,
+    coming: String
+
+  })
+  
+  const Rsvp = mongoose.model('Rsvp', rsvpSchema);
+  
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SEND_GRID)
+  
+  app.post('/rsvp', async (req, res)=>{
+    const saveRsvp = new Rsvp({ ...req.body })
+      const msg = {
+          to: 'rangel_lakers26@hotmail.com', // Change to your recipient
+          from: 'alayna_miracle@outlook.com', // Change to your verified sender
+          subject: `${req.body.name} has RSVP'd for your wedding`,
+          html: `
+          <svg style="width: 50vw; margin: 5rem 25vw;" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+          viewBox="0 0 507.2 507.2" style="enable-background:new 0 0 507.2 507.2;" xml:space="preserve">
+     <circle style="fill:#32BA7C;" cx="253.6" cy="253.6" r="253.6"/>
+     <path style="fill:#0AA06E;" d="M188.8,368l130.4,130.4c108-28.8,188-127.2,188-244.8c0-2.4,0-4.8,0-7.2L404.8,152L188.8,368z"/>
+     <g>
+         <path style="fill:#FFFFFF;" d="M260,310.4c11.2,11.2,11.2,30.4,0,41.6l-23.2,23.2c-11.2,11.2-30.4,11.2-41.6,0L93.6,272.8
+             c-11.2-11.2-11.2-30.4,0-41.6l23.2-23.2c11.2-11.2,30.4-11.2,41.6,0L260,310.4z"/>
+         <path style="fill:#FFFFFF;" d="M348.8,133.6c11.2-11.2,30.4-11.2,41.6,0l23.2,23.2c11.2,11.2,11.2,30.4,0,41.6l-176,175.2
+             c-11.2,11.2-30.4,11.2-41.6,0l-23.2-23.2c-11.2-11.2-11.2-30.4,0-41.6L348.8,133.6z"/>
+     </g>
+     </svg>
+     <h1 style="font: 600 2rem Arial; color: #242424; width: 100%; text-align: center;">HEY CRISTA!</h1>
+     <p style="font: 300 1.5rem Arial; color: #0AA06E; width: 100%; text-align: center;"><span style="color: #dc2f02; font: inherit;">${req.body.name}</span> has just rsvp'd for your wedding with a count of <span style="color: #dc2f02; font: inherit;">${req.body.count}</span> . 
+    They said about coming: <span style="color: #dc2f02; font: inherit;">${req.body.coming}</span> </p>
+    <div style="font: 300 1.2rem Arial; color: #242424; width: 100%; text-align: center;">If they left a message it will be displayed below.</div>
+    <p style="font: 300 1.2rem Arial; color: #242424; width: 100%; text-align: center;">${req.body.message}</p>
+ `
+        }
+        sgMail
+        .send(msg)
+        .then(() => {
+          console.log(msg)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+  
+        try {
+          await saveRsvp.save()
+          res.redirect('/thank-you')
+          
+        } catch (error) {
+          console.log(error)
+        }
+  })
+  
+  app.get('/rsvp', async (req, res)=>{
+  const data = await Rsvp.find({})
+  res.send(data)
+  })
+  
+  app.get('/', (req, res)=>{
+      res.send('index.html')
+  })
+  app.get('/thank-you', (req, res)=>{
+    res.sendFile(`${__dirname}/public/thankyou.html`)
+  })
+  
+  
+  
+  console.log(process.env.SEND_GRID)
+  
+  app.listen(process.env.PORT || 5000, (error)=>{
+    if (error) return console.log(error)
+    console.log(`connected on ${5000}`)
+  })
+  
--- public/assets/js/rsvp-list.js
+++ public/assets/js/rsvp-list.js
@@ -1,9 +1,15 @@
+
 const pullList = async() =>{
   const data =  await fetch('/rsvp');
   const response = await data.json();
   const name = document.querySelector('.nog-top');
   const yesComing = document.querySelector('.y-top');
   const noComing = document.querySelector('.n-top');
+  const number = document.querySelector('.number-of-guest');
+  const total = document.querySelector('#total-guests');
+  let totalCount = 0;
+
+console.log(response)
 
   for (item of response)
   {
@@ -11,24 +17,31 @@ const pullList = async() =>{
     guest.textContent = item.name;
     name.appendChild(guest)
 
-    if(item.coming == 'yes-coming'){
+    const numberr = document.createElement('DIV');
+    numberr.textContent = item.count
+    number.appendChild(numberr)
+
+    if(item.coming == 'I am Coming'){
         const  yes = document.createElement('DIV');
-        yes.textContent = 'YES';
+        yes.textContent = '✅';
         yesComing.appendChild(yes)
         const  no = document.createElement('DIV');
         no.textContent = '.';
         no.style.color = 'rgba(22, 15, 54, 0.8)'
         noComing.appendChild(no)
-    } else if (item.coming == 'no-coming'){
+    } else if (item.coming== "Sorry, I won't be able to come"){
         const  yes = document.createElement('DIV');
         yes.textContent = '.';
         yesComing.appendChild(yes)
         yes.style.color = 'rgba(22, 15, 54, 0.8)'
         const  no = document.createElement('DIV');
-        no.textContent = 'NO';
+        no.textContent = '❌';
         noComing.appendChild(no)
     }
+    totalCount = totalCount + item.count
   }
+  total.textContent = totalCount
+
 }
 
 pullList()
